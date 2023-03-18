fa5fec9257548eeae8f00bd14c3f18660d02ad15
Ilya misyura <ilya.mi@digitrade.co.il>
2023-03-18 11:26:06 +0200

    add Review

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { faker } from '@faker-js/faker'
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import { ReviewDto } from './review.dto'
+import { returnReviewObject } from './return-review.object'
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService {
+	constructor(private prisma: PrismaService) {}
+
+	async getAll() {
+		return this.prisma.review.findMany({
+			orderBy: {
+				createdAt: 'desc'
+			},
+			select: returnReviewObject
+		})
+	}
+
+	async create(userId: number, dto: ReviewDto, productId: number) {
+		return this.prisma.review.create({
+			data: {
+				...dto,
+				product: {
+					connect: {
+						id: productId
+					}
+				},
+				user: {
+					connect: {
+						id: userId
+					}
+				}
+			}
+		})
+	}
+
+	async delete(id: number) {
+		return this.prisma.review.delete({
+			where: {
+				id
+			}
+		})
+	}
+
+	async getAverageValueByProductId(productId: number) {
+		return this.prisma.review
+			.aggregate({
+				where: { productId },
+				_avg: { rating: true }
+			})
+			.then(data => data._avg)
+	}
+}
